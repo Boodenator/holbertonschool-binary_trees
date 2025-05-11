9b35291e01513b9cad4834bac8a6c577e959d037
Boodenator <boode1000@gmail.com>
2025-05-12 00:41:40 +0300

    insert right node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - node insertion as left-aux
+ * binary_tree_insert_right - node insertion as right-aux
  * into other in a binary tree.
  * @parent: ptr to node for inserttion of left-aux.
  * @value: The new node stored vlaue.
@@ -11,23 +11,33 @@
  *
  */
 
- binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
 	if (parent == NULL)
-		return (NULL);
-
-	node = binary_tree_node(parent, value);
-	if (node == NULL)
-		return (NULL);
-
-	if (parent->left != NULL)
 	{
-		node->right = parent->right;
-		parent->right->parent = node;
+		return (NULL);
 	}
-	parent->right = node;
 
-	return (node);
+node = malloc(sizeof(*node));
+if (node == NULL)
+{
+	return (NULL);
+}
+
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
+
+if (parent->right != NULL)
+{
+	node->right = parent->right;
+	parent->right->parent = node;
+}
+
+parent->right = node;
+
+return (node);
 }
