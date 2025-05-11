4d07ba78c778351d77eef4809119d7f9678b5d28
Boodenator <boode1000@gmail.com>
2025-05-11 23:26:50 +0300

    creates new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(parent));
+	node = malloc(sizeof(*node));
 	if (node == NULL)
 		return (NULL);
 
