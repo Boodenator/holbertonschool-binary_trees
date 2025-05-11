70a3d5200f6362683b2c0256de479abd8dc52d3a
Boodenator <boode1000@gmail.com>
2025-05-11 15:01:49 +0300

    create node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - new binary tree node creation.
+ * @parent: Ptr to parent of the newly node.
+ * @value: The inserted value in the new node.
+ *
+ * Return: If an error occurs - NULL.
+ *         Else - ptr to the new node.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(parent));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
